0a8e21978e860423d79286d89617ea7bc91693f7
sahraozdl <seviyyenozdil@gmail.com>
2025-03-03 17:14:39 +0300

    added entry components for main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import Header from "./components/Header.jsx";
+import Header from "./components/Header"
+import Entry from "./components/Entry"
+import data from "./data"
 
 export default function App() {
-  return (
-    <>
-      <Header />
-    </>
-  );
+    
+    const entryElements = data.map((entry) => {
+        return (
+            <Entry
+                key={entry.id}
+                entry={entry}
+            />
+        )
+    })
+    
+    return (
+        <>
+            <Header />
+            <main className="container">
+                {entryElements}
+            </main>
+        </>
+    )
 }
