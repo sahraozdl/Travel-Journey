e6fc7f9ad8d2a92a3b0ed568260557940ed7df6f
sahraozdl <seviyyenozdil@gmail.com>
2025-10-01 01:41:07 +0300

    updated location search

--- src/components/LocationSearch.jsx
+++ src/components/LocationSearch.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from "react";
 import { useLoadGoogleMaps } from "./useLoadGoogleMaps";
 
 const LocationSearch = ({ onPlaceSelected }) => {
-  const containerRef = useRef(null);
+  const inputRef = useRef(null);
 
   useLoadGoogleMaps();
 
@@ -13,17 +13,15 @@ const LocationSearch = ({ onPlaceSelected }) => {
         return;
       }
 
-      const autocompleteEl = document.createElement("google-places-autocomplete");
+      const autocomplete = new window.google.maps.places.Autocomplete(inputRef.current);
 
-      autocompleteEl.addEventListener("place_changed", () => {
-        const place = autocompleteEl.getPlace();
+      autocomplete.addListener("place_changed", () => {
+        const place = autocomplete.getPlace();
         if (onPlaceSelected && place.place_id) {
           const mapUrl = `https://www.google.com/maps/place/?q=place_id:${place.place_id}`;
           onPlaceSelected(mapUrl);
         }
       });
-
-      containerRef.current.appendChild(autocompleteEl);
     };
 
     const interval = setInterval(() => {
@@ -37,13 +35,15 @@ const LocationSearch = ({ onPlaceSelected }) => {
   }, [onPlaceSelected]);
 
   return (
-  <input
-    ref={inputRef}
-    type="text"
-    placeholder="Search for a location"
-    className="input w-full"
-  />
-);
+    <div className="w-full">
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="Search for a location"
+        className="input w-full border rounded p-2"
+      />
+    </div>
+  );
 };
 
 export default LocationSearch;
